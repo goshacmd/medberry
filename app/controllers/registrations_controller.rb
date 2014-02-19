bd668d6c0c442000f11c6946d0b1ec0b6cf5732a
Gosha Arinich <me@goshakkk.name>
2014-02-19 20:14:19 +0300

    move patient data to a user subclass

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    devise_parameter_sanitizer.send(:default_params).permit(:bsn, :dob, :gender, :first_name, :last_name, :zip, :address, :city)
+    devise_parameter_sanitizer.send(:default_params).permit(:bsn, :dob, :gender, :first_name, :last_name, :zip, :address, :city, :email, :password, :password_confirmation)
+  end
+
+  def resource_class
+    Patient
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :bsn, presence: true, length: { is: 9 }, format: { with: /\A[0-9]+\z/ }
-  validates :dob, presence: true
-  validates :gender, presence: true, inclusion: { in: %w(male female) }
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :zip, presence: true
-  validates :address, presence: true
-  validates :city, presence: true
 end
