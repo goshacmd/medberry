f58cf1f1a9b322f8bbf52a6f4ef9cc64fd2848a7
Gosha Arinich <me@goshakkk.name>
2014-02-22 18:27:59 +0300

    expose better_errors from docker to host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Medlive::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  BetterErrors::Middleware.allow_ip! '172.16.42.1' # docker via docker-osx
 end
