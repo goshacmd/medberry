de85399e5f027a348bc55718b78ec5427baef937
Gosha Arinich <me@goshakkk.name>
2014-03-10 15:18:11 +0300

    per consultation channels

--- app/assets/javascripts/routes/consultation_request.js
+++ app/assets/javascripts/routes/consultation_request.js
@@ -1,15 +1,12 @@
 Medlive.ConsultationRequestRoute = Ember.Route.extend({
   enter: function() {
-    channel.bind('consultation_started', this.showConsultation.bind(this));
+    var eventName = 'requests:' + this.currentModel.get('id');
+    channel.bind(eventName, this.showConsultation.bind(this));
   },
 
   showConsultation: function(data) {
-    if (data.request !== this.currentModel.get('id')) return;
-
-    var self = this;
-
     this.store.find('consultation', data.consultation).then(function(consultation) {
-      self.transitionTo('consultation', consultation);
-    });
+      this.transitionTo('consultation', consultation);
+    }.bind(this));
   }
 });
--- app/services/consultation_dispatcher.rb
+++ app/services/consultation_dispatcher.rb
@@ -5,10 +5,6 @@ class ConsultationDispatcher
     @request = request
   end
 
-  def push(channels, data)
-    Pusher.trigger(channels, 'consultation_started', data)
-  end
-
   def perform
     consultation = Consultation.create_from_request(request)
 
@@ -17,6 +13,6 @@ class ConsultationDispatcher
     request.update(status: :accepted)
 
     channels = [consultation.patient, consultation.doctor].map { |i| i.user.try(:pusher_channel_name) }
-    push channels, request: request.id, consultation: consultation.id
+    Pusher.trigger(channels, "requests:#{request.id}", consultation: consultation.id)
   end
 end
