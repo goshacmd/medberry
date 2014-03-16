6535787ac395d62daa46fd40c360e765e4a4a434
Gosha Arinich <me@goshakkk.name>
2014-03-16 14:07:19 +0300

    only create consultation request if the doctor is online

--- app/services/consultation_request_creator.rb
+++ app/services/consultation_request_creator.rb
@@ -11,8 +11,9 @@ class ConsultationRequestCreator
   def perform
     consultation_request = ConsultationRequest.new params
     consultation_request.patient = current_user.identity
+    status = DoctorStatusChecker.new(consultation_request.doctor).check
 
-    if consultation_request.save
+    if status == :online && consultation_request.save
       Pusher.trigger consultation_request.doctor_channel, "requests", ConsultationRequestSerializer.new(consultation_request).as_json
       consultation_request
     else
