56b205df819c904cc20604d251dc12aa660fa1c4
Gosha Arinich <me@goshakkk.name>
2014-03-21 18:47:56 +0300

    customizable self video position

--- app/assets/javascripts/components/tokbox-video.js
+++ app/assets/javascripts/components/tokbox-video.js
@@ -1,34 +1,51 @@
+var send = function(self, eventName) {
+  return function() {
+    var newArguments = [eventName].concat(Array.prototype.slice.call(arguments, 0));
+    Ember.run(function() {
+      self.send.apply(self, newArguments)
+    });
+  };
+};
+
+var listenFor = function(object, self, eventName) {
+  object.addEventListener(eventName, send(self, eventName))
+};
+
+var listenTo = function(object, self, events) {
+  events.forEach(function(eventName) {
+    listenFor(object, self, eventName);
+  });
+};
+
+var publisherEvents = ['accessAllowed', 'accessDenied', 'accessDialogOpened', 'accessDialogClosed'];
+var sessionEvents = ['connectionCreated', 'connectionDestroyed', 'sessionConnected', 'sessionDisconnected', 'signal', 'streamCreated', 'streamDestroyed', 'streamPropertyChanged'];
+
+var selfId = 'video-self';
+var selfSel = '#video-self';
+var mateId = 'video-mate';
+var mateSel = '#video-mate';
+
 App.TokboxVideoComponent = Ember.Component.extend({
   sessionId: null,
   token: null,
   publisher: null,
   session: null,
   cameraAccessError: null,
+  selfPosition: 4, // 1 - top left, 2 - top right, 3 - bottom left, 4 - bottom right
 
   mateStreamId: null,
 
+  mate$: function() {
+    return this.$(mateSel);
+  },
+
+  self$: function() {
+    return this.$(selfSel);
+  },
+
   setupEventListeners: function(publisher, session) {
-    var self = this;
-
-    var send = function(eventName) {
-      return function() {
-        var newArguments = [eventName].concat(Array.prototype.slice.call(arguments, 0));
-        Ember.run(function() {
-          self.send.apply(self, newArguments)
-        });
-      };
-    };
-
-    var listenFor = function(object, eventName) { object.addEventListener(eventName, send(eventName)) };
-
-    var listenTo = function(object, events) {
-      events.forEach(function(eventName) {
-        listenFor(object, eventName);
-      });
-    };
-
-    listenTo(publisher, ['accessAllowed', 'accessDenied', 'accessDialogOpened', 'accessDialogClosed']);
-    listenTo(session, ['connectionCreated', 'connectionDestroyed', 'sessionConnected', 'sessionDisconnected', 'signal', 'streamCreated', 'streamDestroyed', 'streamPropertyChanged']);
+    listenTo(publisher, this, publisherEvents);
+    listenTo(session, this, sessionEvents);
   },
 
   setupTokbox: function() {
@@ -39,7 +56,7 @@ App.TokboxVideoComponent = Ember.Component.extend({
     this.setVideoSizes();
     this.positionVideoElements();
 
-    var publisher = this.publisher = TB.initPublisher(apiKey, 'video-self');
+    var publisher = this.publisher = TB.initPublisher(apiKey, selfId)
     var session = this.session = TB.initSession(sessionId);
 
     this.setupEventListeners(publisher, session);
@@ -58,7 +75,7 @@ App.TokboxVideoComponent = Ember.Component.extend({
   },
 
   setMateSize: function(size) {
-    var mate$ = this.$('#video-mate');
+    var mate$ = this.mate$();
 
     this.mateWidth = size.width;
     this.mateHeight = size.height;
@@ -67,7 +84,7 @@ App.TokboxVideoComponent = Ember.Component.extend({
   },
 
   setSelfSize: function(size) {
-    var self$ = this.$('#video-self');
+    var self$ = this.self$();
 
     this.selfWidth = size.width;
     this.selfHeight = size.height;
@@ -79,23 +96,39 @@ App.TokboxVideoComponent = Ember.Component.extend({
     var mateSize = this.computeOptimalMateVideoSize();
     this.setMateSize(mateSize);
 
-    var self$ = this.$('#video-self');
-
     var selfSize = { width: 264, height: 198 };
     this.setSelfSize(selfSize);
   },
 
+  computeSelfVideoPosition: function() {
+    var pos = parseInt(this.get('selfPosition'));
+
+    var mate$ = this.mate$();
+    var self$ = this.self$();
+    var k = 20;
+
+    var left = pos == 1 || pos == 3 ? k : mate$.width() - self$.width() - k;
+    var top = pos == 1 || pos == 2 ? k : mate$.height() - self$.height() - k;
+
+    return { left: left, top: top };
+  },
+
+  computeMateVideoPosition: function() {
+    return { top: -this.self$().height() };
+  },
+
   positionVideoElements: function() {
-    var mate$ = this.$('#video-mate');
-    var mateWidth = mate$.width();
-    var mateHeight = mate$.height();
+    var mate$ = this.mate$();
+    var self$ = this.self$();
 
-    var self$ = this.$('#video-self');
-    var selfWidth = self$.width();
-    var selfHeight = self$.height();
+    mate$.css({ position: 'relative' });
+    self$.css({ position: 'relative', 'z-index': 100 });
 
-    mate$.css({ position: 'relative', top: -selfHeight });
-    self$.css({ position: 'relative', left: mateWidth - selfWidth - 20, top: mateHeight - selfHeight - 20, 'z-index': 100 });
+    var matePosition = this.computeMateVideoPosition();
+    var selfPosition = this.computeSelfVideoPosition();
+
+    mate$.css(matePosition);
+    self$.css(selfPosition);
   },
 
   unsubscribeTokbox: function() {
@@ -105,12 +138,14 @@ App.TokboxVideoComponent = Ember.Component.extend({
   subscribeToStreams: function(streams) {
     var self = this;
 
-    var mateStream = this.session.streams.find(function(stream) {
+    var notOwnStream = function(stream) {
       return (stream.connection.connectionId != self.session.connection.connectionId);
-    });
+    };
+
+    var mateStream = this.session.streams.find(notOwnStream);
 
     if (mateStream && this.mateStreamId != mateStream.streamId) {
-      this.session.subscribe(mateStream, 'video-mate', {
+      this.session.subscribe(mateStream, mateId, {
         width: this.mateWidth, height: this.mateHeight
       });
     }
