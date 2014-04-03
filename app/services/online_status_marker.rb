598bf722597794535a911dd44800c1991f7f3b44
Gosha Arinich <me@goshakkk.name>
2014-04-03 16:05:21 +0300

    store status_service on marker & pusher

--- app/services/online_status_marker.rb
+++ app/services/online_status_marker.rb
@@ -1,7 +1,11 @@
 class OnlineStatusMarker
-  def perform
-    status_service = OnlineStatusService.new
+  attr_reader :status_service
 
+  def initialize(status_service: OnlineStatusService.new)
+    @status_service = status_service
+  end
+
+  def perform
     User.all.each do |user|
       status_service.mark(user)
     end
--- app/services/online_status_pusher.rb
+++ app/services/online_status_pusher.rb
@@ -1,7 +1,13 @@
 class OnlineStatusPusher
+  attr_reader :status_service
+
+  def initialize(status_service: OnlineStatusService.new)
+    @status_service = status_service
+  end
+
   # Push the online statuses of all doctors.
   def perform
-    mapper = -> i { { id: i.id, status: OnlineStatusService.new.status(i.user) } }
+    mapper = -> i { { id: i.id, status: status_service.status(i.user) } }
 
     doctors_data = Doctor.all.map(&mapper)
     patients_data = Patient.all.map(&mapper)
