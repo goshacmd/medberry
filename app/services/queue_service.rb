6b067241044403c20e3e6d9d96a3bd9874ec1f10
Gosha Arinich <me@goshakkk.name>
2014-04-03 21:17:05 +0300

    request queue position querying

--- app/services/queue_service.rb
+++ app/services/queue_service.rb
@@ -1,13 +1,28 @@
+# A service for querying the stats of the doctor's queue.
 class QueueService
   attr_reader :doctor
 
+  # Intiialize a new +QueueService+.
+  #
   # @param doctor [Doctor]
   def initialize(doctor)
     @doctor = doctor
   end
 
+  # Get a set of consultation requests.
+  def unfilled_requests
+    doctor.consultation_requests.unfilled.order(created_at: :asc)
+  end
+
   # @return [ConsultationRequest, nil]
   def next_request
-    doctor.consultation_requests.unfilled.order(created_at: :asc).first
+    unfilled_requests.first
+  end
+
+  # Get the position of a request in the queue.
+  #
+  # @return [Integer]
+  def position_in_queue(request)
+    unfilled_requests.index(request)
   end
 end
