183f29ee64e35f9c5e0497d768764645b731625e
Gosha Arinich <me@goshakkk.name>
2014-04-16 17:30:35 +0300

    doctor consultation policy values are in minutes

--- app/models/doctor_consultation_policy.rb
+++ app/models/doctor_consultation_policy.rb
@@ -1,7 +1,7 @@
 class DoctorConsultationPolicy < Struct.new(:initial_span, :max_extensions, :extension_span)
   POLICIES = {
-    family: [20, 1, 10],
-    pharmacist: [20, 1, 10]
+    family: [20.minutes, 1, 10.minutes],
+    pharmacist: [20.minutes, 1, 10.minutes]
   }
 
   def self.for_practice(type)
