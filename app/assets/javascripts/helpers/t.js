1e6daa4e0d276a9859939301e698737553cf2adc
Gosha Arinich <me@goshakkk.name>
2014-04-17 15:05:12 +0300

    use #helper instead of #registerHelper

--- app/assets/javascripts/helpers/t.js
+++ app/assets/javascripts/helpers/t.js
@@ -1,10 +1,6 @@
-Ember.Handlebars.registerHelper('t', function(property, options) {
+Ember.Handlebars.helper('t', function(property, options) {
   var self = this;
   var params = options.hash;
-
-  Object.keys(params).forEach(function (key) {
-    params[key] = Em.Handlebars.get(self, params[key], options);
-  });
 
   return I18n.t('js.' + property, params);
 });
