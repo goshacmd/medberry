b25070e27b3b34ac5602f665273a1a9384e0cbf4
Gosha Arinich <me@goshakkk.name>
2014-04-17 16:13:25 +0300

    recompile assets

--- public/assets/application-f5426ddf0ef41a460af9555c1c73616e.js
+++ public/assets/application-f5426ddf0ef41a460af9555c1c73616e.js
@@ -73148,6 +73148,12 @@ Ember.Handlebars.helper('t', function(property, options) {
   var self = this;
   var params = options.hash;
 
+  Object.keys(params).forEach(function (key) {
+    if (key == 'boundOptions') return;
+
+    params[key] = Em.Handlebars.get(self, params[key], options);
+  });
+
   return I18n.t('js.' + property, params);
 });
 Ember.Handlebars.helper('time', function(diff) {
