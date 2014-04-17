541acf6e0992e1b55afdf779f29ecec44f200b48
Gosha Arinich <me@goshakkk.name>
2014-04-17 16:12:28 +0300

    fix bound options in translation helper

--- app/assets/javascripts/helpers/t.js
+++ app/assets/javascripts/helpers/t.js
@@ -2,5 +2,11 @@ Ember.Handlebars.helper('t', function(property, options) {
   var self = this;
   var params = options.hash;
 
+  Object.keys(params).forEach(function (key) {
+    if (key == 'boundOptions') return;
+
+    params[key] = Em.Handlebars.get(self, params[key], options);
+  });
+
   return I18n.t('js.' + property, params);
 });
